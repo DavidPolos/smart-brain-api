3e2890d9dac48f2fb4c576ea1da53ec3662bfc47
David Polos <polosdav@gmail.com>
2022-06-17 14:57:43 +0200

    added sessions45ed-

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://big-brain-smart.herokuapp.com/',
     credentials: true,
 }))
   
